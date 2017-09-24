2c31c05ac0b583e2b6fb1db4a641491a2fc79419
Joseph Butler <joebutlerit@gmail.com>
2017-09-24 19:09:58 -0400

    update

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -9,7 +9,20 @@
 namespace AppBundle\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+
 class GenusController
 {
+
+    /**
+     * @Route("/genus")
+     */
+    public function showAction()
+    {
+        return new Response('Under the Seat!');
+    }
+
 
 }
